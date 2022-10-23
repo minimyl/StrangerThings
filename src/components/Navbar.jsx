78974907c69438c76b2dbb573dd8dc4636ef7925
Andrew <root@minimyl.localdomain>
2022-10-23 19:21:37 -0400

    added the build and redirects and made the home page go to the posts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -10,6 +10,9 @@ const Navbar = () => {
       <div>
         <Outlet />
       </div>
+      <Link to={`/posts`}>
+        <button className="postDetailsButton">Go to Posts Page</button>
+      </Link>
     </>
   );
 };
